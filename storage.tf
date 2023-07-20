68ed3b10f072414e612a89e5393e9e331b5109b7
Nibin Kunnumpurath Joseph <nibinjoseph@Nibins-MBP.lan>
2023-07-21 05:49:31 +1000

    storage edit

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "njstorageApache"
+  name                     = "njstorage21"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
